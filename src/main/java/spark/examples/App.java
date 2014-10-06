12ef399cc56b330662591bfe099c9c183cf3f96c
Ivan Fernandez <ivan.fernandez@lambdoop.com>
2014-10-06 02:07:28 -0700

    First commit

---
+++ src/main/java/spark/examples/App.java
@@ -0,0 +1,13 @@
+package spark.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
